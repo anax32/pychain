2699bb70bf8b86383ae50279fb70bd5020076da8
ed <anax@hotmail.co.uk>
2021-01-17 17:34:19 +0000

    added initial tests

---
+++ pychain/tests/test_chain.py
@@ -0,0 +1,19 @@
+import pychain
+
+
+def test_create_chain_creates_genesis_block():
+  x = pychain.Chain()
+
+  assert x is not None
+  assert len(x) == 1
+
+
+def test_append_item_increase_chain_length():
+  x = pychain.Chain()
+
+  assert x is not None
+  assert len(x) == 1
+
+  x.append("a")
+
+  assert len(x) == 2
